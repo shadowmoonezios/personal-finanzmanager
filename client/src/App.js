38fdb5997743472d0a7785f66cf3a4b352079e6b
shadowmoonezios <agushifelde@hotmail.com>
2025-09-25 17:45:47 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Personal Finanzmanager</h1>
+      <p>Willkommen beim Personal Finanzmanager! Verwalten Sie Ihre Finanzen.</p>
+    </div>
+  );
+}
+
+export default App;
